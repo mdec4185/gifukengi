a752e94414a4ed25f32e364e8c9fc8d404eb3109
gifukengi <mdec@mui.biglobe.ne.jp>
2025-01-12 21:11:00 +0900

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', function(event) {
-    console.log('Service Worker installed.');
-});
-
-self.addEventListener('fetch', function(event) {
-    // キャッシュ機能が必要であればここで実装可能
-});
